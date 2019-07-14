25d7a17a4723747f41dbda4c7843469d4b3f7afe
edumoritz <eduardo.moritz@hotmail.com>
2019-07-14 15:56:50 -0300

    ajustando tela de login

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Criar Conta'
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
+  }
+  createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  get name(): FormControl {
+    return <FormControl>this.authForm.get('name');
+  }
+  get email(): FormControl {
+    return <FormControl>this.authForm.get('email');
+  }
+  get password(): FormControl {
+    return <FormControl>this.authForm.get('password');
+  }
+
+  changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Sign Up';
+    this.configs.actionChange = isSignIn ? 'Criar Conta' : 'Já Possuo uma Conta';
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
+  }
+
+  onSubmit(): void {
+    console.log('AuthForm: ', this.authForm);
+  }
 }
