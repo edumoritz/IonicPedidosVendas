99c1007f844896d42c1ba43b50836f78e35986e1
edumoritz <eduardo.moritz@hotmail.com>
2019-07-20 14:42:41 -0300

    orquestrando login

---
+++ src/app/core/service/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { auth } from 'firebase';
+
+import { User, AuthProvider, AuthOptions } from './auth.types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private afAuth: AngularFireAuth) {}
+
+  authenticate({ isSignIn, provider, user }: AuthOptions): Promise<auth.UserCredential> {
+    let operation: Promise<auth.UserCredential>;
+
+    if (provider !== AuthProvider.Email) {
+      operation = this.signInWithPopup(provider);
+    } else {
+      operation = isSignIn ? this.signInWithEmail(user) : this.signUpWithEmail(user);
+    }
+
+    return operation;
+  }
+
+  private signInWithEmail({ email, password }: User): Promise<auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  private signUpWithEmail({ email, password, name }: User): Promise<auth.UserCredential> {
+    return this.afAuth.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(credentials =>
+        credentials.user
+          .updateProfile({ displayName: name, photoURL: null })
+          .then(() => credentials)
+      );
+  }
+
+  private signInWithPopup(provider: AuthProvider): Promise<auth.UserCredential> {
+    let signInProvider = null;
+
+    switch (provider) {
+      case AuthProvider.Facebook:
+        signInProvider = new auth.FacebookAuthProvider();
+        break;
+    }
+
+    return this.afAuth.auth.signInWithPopup(signInProvider);
+  }
+}
