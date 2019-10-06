928b529086390176f63a199990110f508d855f2d
edumoritz <eduardo.moritz@hotmail.com>
2019-10-06 19:51:05 -0300

    menu toggle + logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   pages: { url: string; direction: string; icon: string; text: string }[];
+  user: firebase.User;
 
   constructor(
+    private authService: AuthService,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -24,6 +27,8 @@ export class AppComponent {
       { url: '/requests', direction: 'back', icon: 'checkmark', text: 'Pedidos' },
       { url: '/requests/create', direction: 'forward', icon: 'add', text: 'Novo Pedido' }
     ];
+
+    this.authService.authState$.subscribe(user => (this.user = user));
 
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
